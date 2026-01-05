e844a93ba0ff319f6e86317f28165b8273e56288
Constant <110848773+joaopconstant@users.noreply.github.com>
2026-01-05 19:30:40 -0300

    Update whitelist message ID

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const config = {
   port: process.env.PORT || 3000,
 
   // Bot configuration constants
-  targetMessageId: "1457522794920153315",
+  targetMessageId: "1457863518874243125",
   targetRoleId: "1423494445805867148",
   roleToRemoveId: "1454110873504190506",
   targetEmoji: "✅", // :white_check_mark:
